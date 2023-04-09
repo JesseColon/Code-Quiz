c8d9731e282d91e30a92c6e7ebaefee7e33c2b10
AzureSol <jay.alexandervii@gmail.com>
2023-04-08 20:41:10 -0400

    added css

---
+++ Assets/script.js
@@ -0,0 +1,4 @@
+var timer = document.getElementById("timer-count")
+var highScore = document.getElementById("high-score-count")
+var start = document.getElementById("start-button")
+var score = document.getElementById("current-score")
