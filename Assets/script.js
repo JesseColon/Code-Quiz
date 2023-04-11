87258e5ce1a2c65c9364eddb9bca76ed37f7cc5d
unknown <jay.alexandervii@gmail.com>
2023-04-11 18:06:42 -0400

    added questions

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,93 @@
-var timer = document.getElementById("timer-count")
-var highScore = document.getElementById("high-score-count")
-var start = document.getElementById("start-button")
-var score = document.getElementById("current-score")
+var startEl = document.querySelector("#start-button")
+var timerEl = document.querySelector("#timer-count")
+var scoreEl = document.querySelector("current-score")
+var highScoreEl = document.querySelector(".high-score-count")
+
+var timer;
+var timerCount;
+var timeInterval;
+var highScoreCounter = 0;
+var scoreCount = 0;
+
+// create object of question functions
+
+var questions = [question1, question2, question3];
+
+function question1 (){
+    question: ""
+    answer:{
+        a:""
+        b:""
+        c:""
+        d:""
+    };
+
+}
+
+function question2 (){
+    question: 
+    answer:{""
+        a:""
+        b:""
+        c:""
+        d:""
+    };
+
+}
+
+function question3 (){
+    question: 
+    answer:{""
+        a:""
+        b:""
+        c:""
+        d:""
+    };
+
+}
+
+
+function startQuiz() {
+   console.log("print when clicked");
+   timerCount = 90;
+   startEl.disabled = true;
+    countDownStart()
+    // qusetionStart()
+}
+
+
+
+function countDownStart () {
+
+    timer = setInterval(function() {
+
+        timerCount--;
+        
+        timerEl.textContent = timerCount;
+
+        if (timerCount >= 0) {
+
+          if (iswin = false && timerCount > 0){
+
+            clearInterval(timer);
+
+            }
+
+        } if(timerCount === 0) {
+
+            timerEl.textContent = " ";
+            clearInterval(timer)
+            gameover()
+        }
+    }, 1000);
+
+
+};
+
+function qusetionStart () {
+
+
+}
+
+startEl.addEventListener("click", startQuiz);
+
